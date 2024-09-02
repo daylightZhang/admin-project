cda88104d5f628c503d0a18a89a42c96d5444945
jingkaizhang <jz544@cornell.edu>
2024-09-02 22:03:32 +0800

    🔧 add: mock config

--- vite.config.ts
+++ vite.config.ts
@@ -1,31 +1,39 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
-import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueJsx from "@vitejs/plugin-vue-jsx";
+import { createSvgIconsPlugin } from "vite-plugin-svg-icons";
+import { viteMockServe } from "vite-plugin-mock"
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-    createSvgIconsPlugin({
-      iconDirs: [fileURLToPath(new URL('./src/assets/icons/svg', import.meta.url))],
-      symbolId: 'icon-[dir]-[name]'
-    })
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig(({ command }) => {
+  return {
+    plugins: [
+      vue(),
+      vueJsx(),
+      createSvgIconsPlugin({
+        iconDirs: [
+          fileURLToPath(new URL("./src/assets/icons/svg", import.meta.url)),
+        ],
+        symbolId: "icon-[dir]-[name]",
+      }),
+      viteMockServe({
+        localEnabled: command === 'serve',
+      }) // Add the type assertion here
+    ],
+    resolve: {
+      alias: {
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
+      },
+    },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          javascriptEnabled: true,
+          additionalData: `@import "@/style/variables.scss";`,
+        },
+      },
     }
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        javascriptEnabled: true,
-        additionalData: `@import "@/style/variables.scss";`
-      }
-    }
-  }
-})
+  };
+});
